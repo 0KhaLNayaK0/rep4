83f712950cba88ff7481ffbc1012ce793e9ab598
0KhaLNayaK0 <115925245+0KhaLNayaK0@users.noreply.github.com>
2022-10-16 16:19:18 +0530

    Add files via upload

---
+++ circlearea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    // This code is to calculate the area of a circle
+    int a;
+    float b=3.14;
+    printf("Enter the radius of circle \n");
+    scanf("%d" , &a );
+    printf("The area of the circle is %f " , a*a*b );
+    return 0;
+}
